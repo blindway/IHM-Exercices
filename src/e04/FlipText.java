c51c877c4e5740318aa7d2246c99515c74f0e5d4
blindway <blindway@hotmail.fr>
2015-03-27 09:26:11 +0100

    E04

---
+++ src/e04/FlipText.java
@@ -0,0 +1,15 @@
+package e04;
+
+import javafx.application.Application;
+
+
+public class FlipText {
+	
+	public static void main(String[] args) {
+		
+		Application.launch(FlipTextView.class,args);
+		
+	}
+	
+	
+}
