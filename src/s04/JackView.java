06eeea527f5dee06a34fdbc4309a2279450b4df3
blindway <blindway@hotmail.fr>
2015-06-01 16:13:02 +0200

    Fxml okay

--- src/s04/JackView.java
+++
@@ -1,5 +0,0 @@
-package s04;
-
-public class JackView {
-
-}
--- src/s04/view/BlackJackView.java
+++ src/s04/view/BlackJackView.java
@@ -4,7 +4,7 @@ import s04.controller.BlackJackCtrl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class BlackJackView extends Application{
@@ -14,7 +14,7 @@ public class BlackJackView extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("BlackJackView.fxml"));
-		BorderPane root = loader.load();
+		Pane root = loader.load();
 		loader.setController(ctrl);
 		
 		Scene scene = new Scene(root);
