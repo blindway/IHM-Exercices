cd68db68a50659a36dfd56de0ac205235849ef56
blindway <blindway@hotmail.fr>
2015-06-15 15:58:28 +0200

    s04 icone

--- src/s04/view/BlackJackView.java
+++ src/s04/view/BlackJackView.java
@@ -4,33 +4,34 @@ import s04.controller.BlackJackCtrl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class BlackJackView extends Application{
-	
+public class BlackJackView extends Application {
+
 	BlackJackCtrl ctrl = new BlackJackCtrl();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("BlackJackView.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(
+				"BlackJackView.fxml"));
 		Pane root = loader.load();
 		loader.setController(ctrl);
-		
+
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-		
+
 		primaryStage.setResizable(false);
-		
-		primaryStage.show();
 		primaryStage.setTitle("BlackJack");
+		primaryStage.getIcons().add(new Image("s04/view/img/pico.png"));
+
+		primaryStage.show();
 
 	}
-	
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-	
-	
 
 }
